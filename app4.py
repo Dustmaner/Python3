f71b4385ca58a4436db78452ad95482981470c3d
Dustmaner <erick.soy@hotmail.com>
2018-09-23 04:22:01 -0500

    "Advanced" math functions and Getting input from the user

--- app4.py
+++ app4.py
@@ -1,6 +1,11 @@
+from math import *
+
 my_first_num = 1337
 my_second_num = 420
 my_third_num = 1969
 
 print(str(my_first_num) + " + " + str(my_second_num) + " = " + str(my_first_num + my_second_num))
 
+print(floor(3.7))
+print(ceil(3.7))
+print(round(sqrt(36)))
