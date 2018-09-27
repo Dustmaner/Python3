70964e84815cdc9da3eb887880ed76816fd845bd
Dustmaner <erick.soy@hotmail.com>
2018-09-27 15:46:39 -0500

    For loops

---
+++ For_Loop.py
@@ -0,0 +1,9 @@
+friends = ["Djeeta", "Gran", "Vyrn", "Lyria"]
+for index in friends:
+    print(index)
+
+for index in range(len(friends)):
+    print(index)
+
+for letter in "Uriel":
+    print(letter)
