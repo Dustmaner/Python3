7c538ec340e788308bfbb4b41a565da56c723f53
Dustmaner <erick.soy@hotmail.com>
2018-10-17 16:51:05 -0500

    Writing to files

---
+++ Write_File.py
@@ -0,0 +1,17 @@
+#employee_file = open("employees1.txt", "w")
+
+employee_file = open("index.html", "w")
+
+employee_file.write("<p>This is HTML</p>")
+
+employee_file.close()
+
+'''
+Altair - Bookman
+Vajra - Pupperina
+Narumeia - Butterfly
+Quatre - Daggerboi
+Miyuki - Neechan
+Grea - Dragon Trainer
+Europa - Moonman
+'''
