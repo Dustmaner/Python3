a3b65df0417977351bf6f6aed19c8bfed178893d
Dustmaner <erick.soy@hotmail.com>
2018-10-17 18:02:11 -0500

    Classes and Objects uses Student.py

---
+++ Classes&Objects.py
@@ -0,0 +1,9 @@
+from Student import Student
+
+student1 = Student("Anne", "Mahou", 4.0, False)
+student2 = Student("Grea", "Dragon", 3.58, False)
+
+print(student1.name)
+print(student1.gpa)
+
+print(student2.gpa)
